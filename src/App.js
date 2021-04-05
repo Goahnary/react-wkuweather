a8678c27081aafeac1da26ad42e6288bf4eea63a
Noah Gary <noah.gary@bytecode.io>
2021-04-05 03:05:01 -0400

    Created react app and added bootstrap.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Example from './components/example'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Example></Example>
+      </header>
+    </div>
+  );
+}
+
+export default App;
